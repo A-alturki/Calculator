9fe62a50d3d8909266b8a8cc9363b8f40de78390
A-alturki <aboody3553@outlook.com>
2023-07-27 23:28:28 +0300

    made calculator functional

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 const add = function (a, b) {
-    return a + b;
+    return Number(a)  + Number(b);
 };
 
 const subtract = function (a, b) {
@@ -19,49 +19,84 @@ let num2 = 0;
 let operator = "";
 let displayValue = 0;
 const display = document.querySelector('.display');
-function operate(num1, num2, operator){
+let operatorHover = false;
+let operatorHoverobj = null;
+let operationDone = false;
+
+
+function operate(n1, n2, operator){
+    operationDone = true;
     if(operator =="+"){
-        return add(num1,num2);
+        return add(n1,n2);
     }
     else if(operator =="-"){
-        return subtract(num1,num2);
+        return subtract(n1,n2);
     }
     else if(operator =="*"){
-        return multiply(num1,num2);
+        return multiply(n1,n2);
     }
     else if(operator =="/"){
-        return divide(num1,num2);
+        return divide(n1,n2);
     }
     return 0;
 }
 
 
 function populate(e){
+    
+
     if(displayValue.toString().includes(".") && e.textContent == "."){
         return;
     }
-    if(displayValue == 0){
+    if(displayValue == 0 || operatorHover){
         displayValue = e.textContent;
     }
     else{
         displayValue += e.textContent;
     }
     display.textContent = displayValue;
+    operatorHover = false;
+    if(operatorHoverobj){
+        operatorHoverobj.classList.remove("operator-hover");
+    }
     
 }
 
 function depopulate(){
+    operatorHover = false;
+    operatorHoverobj = null;
+    operationDone = false;
     displayValue = 0;
+    num1 = 0;
+    num2 = 0;
+    operator = "";
     display.textContent = displayValue;
+    
 }
 
 function remove(){
+    if(operatorHoverobj){
+        operatorHoverobj.classList.remove("operator-hover");
+    }
     let gru = displayValue.toString();
     displayValue = Number(gru.slice(0,gru.length-1));
     display.textContent = displayValue;
 }
 
-num1 = 5;
-num2 = 10;
-operator = "-";
-console.log(operate(num1,num2,operator));
+
+function op(e){
+    num1 = displayValue;
+    operator = e.textContent;
+
+    operatorHover = true;
+    if(operatorHoverobj){
+        operatorHoverobj.classList.remove("operator-hover");
+    }
+    operatorHoverobj = e;
+    operatorHoverobj.classList.add("operator-hover")
+}
+
+function equal(){
+    displayValue = operate(num1,displayValue, operator);
+    display.textContent = displayValue;
+}
